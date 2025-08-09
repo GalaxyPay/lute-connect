493ecf9035f0247b0a8c81d8037d7cb9fea77d06
Andrew Funk <acfunk@gmail.com>
2025-08-08 19:31:19 -0600

    export base url and ext id

--- src/main.ts
+++ src/main.ts
@@ -112,8 +112,8 @@ export class SignDataError extends Error {
 const left = 100 + window.screenX;
 const top = 100 + window.screenY;
 const PARAMS = `width=500,height=750,left=${left},top=${top}`;
-const BASE_URL = "https://lute.app";
-const EXT_ID = "kiaoohollfkjhikdifohdckeidckokjh";
+export const BASE_URL = "https://lute.app";
+export const EXT_ID = "kiaoohollfkjhikdifohdckeidckokjh";
 
 export default class LuteConnect {
   siteName: string;
